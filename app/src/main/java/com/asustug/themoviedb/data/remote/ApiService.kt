0b0fc3588e35475fdcb50e3027eb2c95c110913f
Boopalan Vadamalai <boopalan457@gmail.com>
2023-02-13 23:59:59 +0900

    paging implementation done

--- app/src/main/java/com/asustug/themoviedb/data/remote/ApiService.kt
+++ app/src/main/java/com/asustug/themoviedb/data/remote/ApiService.kt
@@ -1,5 +1,6 @@
 package com.asustug.themoviedb.data.remote
 
+import com.asustug.themoviedb.data.model.Movie
 import com.asustug.themoviedb.data.model.MovieResponse
 import retrofit2.Response
 import retrofit2.http.*
@@ -7,6 +8,8 @@ import retrofit2.http.*
 interface ApiService {
 
     @GET("trending/movie/day")
-    suspend fun getMoviesList(@Query("api_key") apikey : String) : Response<MovieResponse>
-
+    suspend fun getMoviesList(
+        @Query("api_key") apikey : String,
+        @Query("page") page : Int
+    ) : MovieResponse
 }
--- app/src/main/java/com/asustug/themoviedb/ui/MainViewModel.kt
+++ app/src/main/java/com/asustug/themoviedb/ui/MainViewModel.kt
@@ -3,31 +3,44 @@ package com.asustug.themoviedb.ui
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.asustug.themoviedb.data.model.Movie
 import com.asustug.themoviedb.data.model.MovieResponse
 import com.asustug.themoviedb.repositories.ApiRepository
+import com.asustug.themoviedb.ui.paging.MoviePagingSource
 import com.nec.devicemanagement.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val repository: ApiRepository) :  ViewModel() {
+class MainViewModel @Inject constructor(private val repository: ApiRepository) : ViewModel() {
 
     val user = MutableLiveData<Resource<MovieResponse>>()
 
-    fun fetchUsers(apikey : String) {
+    fun fetchUsers(apikey: String) {
         viewModelScope.launch {
             user.postValue(Resource.loading(data = null))
             try {
-                val response = repository.getMoviesList(apikey)
-                if (response.code() == 200) {
+                val response = repository.getMoviesList(apikey, 1)
+                /*if (response.code() == 200) {
                     user.value = Resource.success(response.body())
                 } else {
                     user.value = Resource.error(response.message().toString(), response.body())
-                }
+                }*/
             } catch (e: Exception) {
                 user.postValue(Resource.error(e.localizedMessage!!.toString(), null))
             }
         }
     }
+
+    fun getAllMovies(): Flow<PagingData<Movie>> = Pager(
+        config = PagingConfig(20, enablePlaceholders = false)
+    ) {
+        MoviePagingSource(repository)
+    }.flow.cachedIn(viewModelScope)
 }
